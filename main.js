2c53af89633ad0b15cb353973106c80c6ec43263
Takumikimura141899 <kimura141899@gmail.com>
2021-05-07 00:03:20 +0900

    challange-finish

--- main.js
+++ main.js
@@ -6,27 +6,32 @@ const text = document.getElementById('add-area');
 const todo = document.getElementById('todo');
 
 
-
 // 追加ボタンの処理
 addBtn.addEventListener('click',() => {
-    // console.log(text.value);
-    // console.log(addBtn.textContent);
-    
-    //<li>を作成
-    const liElement = document.createElement('li');
-    // <li>要素の本文をテキストに入力された本文にする。
-    liElement.textContent = text.value;
-    // 確認ボタン作成
-    const checkBtn = document.createElement('input');
-    checkBtn.type = 'button';
-    checkBtn.value = '確認';
-    // <ul>要素のお尻に<li>要素を足す
-    todo.appendChild(liElement);
-    liElement.appendChild(checkBtn);
-    // 確認ボタンの処理
-    checkBtn.addEventListener('click', () => {
-        // <li>要素毎消す
-        liElement.remove();
-    });
+    // 空欄アラート
+    if (text.value.length < 1) {
+        alert('空欄はダメです');
+    // 一文字以上の時の処理
+    } else {
+        //<li>を作成
+        const liElement = document.createElement('li');
+        // <li>要素の本文をテキストに入力された本文にする。
+        liElement.textContent = text.value;
+        // 確認ボタン作成
+        const checkBtn = document.createElement('input');
+        checkBtn.type = 'button';
+        checkBtn.value = '確認';
+        // <ul>要素のお尻に<li>要素を足す
+        todo.appendChild(liElement);
+        liElement.appendChild(checkBtn);
+        
+        // 確認ボタンの処理　ここが追加ボタンの処理の中に入ってるのが怪しい
+        checkBtn.addEventListener('click', () => {
+            // <li>要素毎消す
+            liElement.remove();
+        });
+    }
+    // テキスト(入力エリア)を空にする。
+    text.value = "";
 });
 
